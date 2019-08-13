52ca14d7973f5064342b09cac0bbfcd039d978d4
Frédéric <f.lugbull@gmail.com>
2019-08-14 00:23:05 +0200

    début exo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :attendances
 	has_many :attended_events, foreign_key: 'attended_event_id', class_name:'Event', through: :attendances
 	has_many :events_administrated, foreign_key: 'admin_id', class_name:'Event'
